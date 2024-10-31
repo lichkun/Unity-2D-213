59086336e9f68fb64b29edaa29385e75bd90a3a1
nikita_lichkun <nikita.lichkun03@gmail.com>
2024-10-31 05:52:19 -0400

    Initial commit

---
+++ Assets/Scripts/DiamondScript.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
