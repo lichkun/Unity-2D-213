eb59cf2273aefe128c0cec1e0b244f59a65aea63
nikita_lichkun <nikita.lichkun03@gmail.com>
2024-11-04 12:06:45 -0500

    add physics and movement to character

--- Assets/Scripts/DiamondScript.cs
+++ Assets/Scripts/DiamondScript.cs
@@ -3,15 +3,46 @@ using UnityEngine;
 
 public class DiamondScript : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, Unity!");
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * 300);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetBool("Walk", true);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb2d.AddForce(Vector2.right * 5);
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                rb2d.AddForce(Vector2.left * 5);
+                spriteRenderer.flipX = true;
+            }
+        }
+        else animator.SetBool("Walk", false);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rb2d.angularVelocity = 0;
+            rb2d.linearVelocity = Vector2.zero;
+        }
     }
 }
